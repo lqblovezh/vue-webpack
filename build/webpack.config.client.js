237400d9f2366b603ea806c25fd28431056a5b77
骆秋波 <1277678760@qq.com>
2019-04-29 15:14:18 +0800

    firest commit

---
+++ build/webpack.config.client.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const baseConfig = require('./webpack.config.base')
+const merge = require('webpack-merge')
+const isDev = process.env.NODE_ENV === 'development'
+const devServer = {
+  port: 8000,
+  host: '0.0.0.0',
+  overlay: {
+    errors: true,
+  },
+  hot: true,
+}
+
+let config
+
+if (isDev) {
+  config = merge(baseConfig, {
+    devtool: '#cheap-module-eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            'stylus-loader',
+          ],
+        },
+      ],
+    },
+    devServer,
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
+    ],
+  })
+} else {
+  config = merge(baseConfig, {
+    entry: {
+      app: path.join(__dirname, '../client/index.js'),
+      vendor: ['vue'],
+    },
+    output: {
+      filename: '[name].[chunkhash:8].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: ExtractPlugin.extract({
+            fallback: 'vue-style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+              'stylus-loader',
+            ],
+          }),
+        },
+      ],
+    },
+    plugins: [
+      new ExtractPlugin('styles.[contentHash:8].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'runtime',
+      }),
+    ],
+  })
+}
+
+module.exports = config
